2dcfb8e6ed9583d5d696e581f834ac2a0048c774
LuzinhaLuz <52301347+DiogoLuz12@users.noreply.github.com>
2022-05-13 12:09:57 +0100

    Criar a class para representar a tabela de categorias (com a função cria para criar a tabela)

---
+++ app/src/main/java/com/example/livros/BDLivrosOpenHelper.kt
@@ -0,0 +1,20 @@
+package com.example.livros
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class BDLivrosOpenHelper(
+    context: Context?,
+    name: String?,
+    factory: SQLiteDatabase.CursorFactory?,
+    version: Int
+) : SQLiteOpenHelper(context, name, factory, version) {
+    override fun onCreate(db: SQLiteDatabase?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        TODO("Not yet implemented")
+    }
+}
